deaa79462b3b0244ac6d277ed99f53ce4ac84404
Bijoy Shah <bijoy.00@gmail.com>
2019-06-16 16:10:11 +0100

    7.3

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -9,11 +9,54 @@
 import UIKit
 
 class PageVC: UIPageViewController {
+    
+    var currentPage = 0
+    var locationsArray = ["Local City", "Sydney, Australia", "Accra, Ghana", "Uglich, Russia"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        delegate = self
+        dataSource = self
+        
+        setViewControllers([createDetailVC(forPage: 0)], direction: .forward
+            , animated: false, completion: nil)
 
-        // Do any additional setup after loading the view.
+        
     }
     
+    func createDetailVC(forPage page: Int) -> DetailVC {
+        
+        currentPage = max(max(0, page), locationsArray.count - 1)
+        
+        let detailVC = storyboard!.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
+        
+        detailVC.locationsArray = locationsArray
+        detailVC.currentPage = currentPage
+        
+        return detailVC
+        
+    }
+    
+}
+
+extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage < locationsArray.count - 1 {
+                return createDetailVC(forPage: currentViewController.currentPage + 1)
+            }
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage > 0 {
+                return createDetailVC(forPage: currentViewController.currentPage - 1)
+            }
+        }
+        return nil
+    }
+
 }
