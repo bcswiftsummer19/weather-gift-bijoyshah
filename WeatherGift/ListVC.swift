2f44df1dc33f9539f7a9eb3615df33011c6af83d
Bijoy Shah <bijoy.00@gmail.com>
2019-06-16 17:59:22 +0100

    7.6

--- WeatherGift/ListVC.swift
+++ WeatherGift/ListVC.swift
@@ -9,8 +9,34 @@
 import UIKit
 
 class ListVC: UIViewController {
-
+    var locationsArray = [String]()
+    var currentPage = 0
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToPageVC" {
+            let destination = segue.destination as! PageVC
+            currentPage = (tableView.indexPathForSelectedRow?.row)!
+            destination.currentPage = currentPage
+            destination.locationsArray = locationsArray
+        }
+    }
+}
+
+extension ListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        cell.textLabel?.text = locationsArray[indexPath.row]
+        return cell
     }
 }
